85e07aba7e50b3b815e25e6c699e18a2fccdfd45
Chace Powell <cpcpowell8@gmail.com>
2018-11-15 21:03:26 -0500

    Updates

---
+++ precache-manifest.193f4792adb0467c1c165b41d3db060a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27aa8ae556ccb3fda5ec",
+    "url": "/clickygame/static/css/main.61b02db5.chunk.css"
+  },
+  {
+    "revision": "27aa8ae556ccb3fda5ec",
+    "url": "/clickygame/static/js/main.27aa8ae5.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/clickygame/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "45d3b01a76c57462a4719f78a19abc4b",
+    "url": "/clickygame/index.html"
+  }
+];
